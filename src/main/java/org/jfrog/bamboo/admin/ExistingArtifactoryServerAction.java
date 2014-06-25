aaae93eea3d65d3f42ee5643f11859a70dfc67ed
liorha <liorha@jfrog.com>
2014-06-25 19:10:51 +0300

    version 2

--- src/main/java/org/jfrog/bamboo/admin/ExistingArtifactoryServerAction.java
+++ src/main/java/org/jfrog/bamboo/admin/ExistingArtifactoryServerAction.java
@@ -16,7 +16,6 @@
 
 package org.jfrog.bamboo.admin;
 
-import bucket.core.actions.PagerPaginationSupport;
 import com.atlassian.bamboo.security.GlobalApplicationSecureObject;
 import com.atlassian.bamboo.ww2.actions.admin.user.AbstractEntityPagerSupport;
 import com.atlassian.bamboo.ww2.aware.permissions.GlobalAdminSecurityAware;
@@ -49,8 +48,8 @@ public class ExistingArtifactoryServerAction extends AbstractEntityPagerSupport 
         return GlobalApplicationSecureObject.INSTANCE;
     }
 
-    @Override
+    /*@Override
     public PagerPaginationSupport getPaginationSupport() {
         return super.getPaginationSupport();
-    }
+    }*/
 }
